00a1eda070485ad9bb0685fc35d01433c906be60
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-18 13:44:55 +0100

    feat: add color to click, not working

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 //Ogni cella ha un numero progressivo, da 1 a 100.
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+//PARTE DUE
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
 
 //seleziono la griglia
 const mainGrid = document.querySelector('#grid');
-
+//creo array vuoto di numeri 'bomba'
+const randomNumbersArray = [];
 //seleziono il pulsante
 const mainButton = document.querySelector('#btn');
 console.log(mainButton)
@@ -16,10 +23,8 @@ mainButton.addEventListener('click', function() {
     //funzione che crea un array di numeri random
     function generateRandomArray(arrayLength, numMin, numMax) {
         // Creiamo un array vuoto
-        const randomNumbersArray = [];
-
         while(randomNumbersArray.length < arrayLength) {
-            // genero un numero random
+            // genero un numero random e controllo che non sia già stato inserito
             const randNumber = getRndInteger(numMin, numMax);
             if(!randomNumbersArray.includes(randNumber)) {
                 randomNumbersArray.push(randNumber);
@@ -35,7 +40,6 @@ mainButton.addEventListener('click', function() {
     const newSquare = generateSquare(i);
     mainGrid.append(newSquare);
 }
-});
 
 //funzione che crea un quadrato
 function generateSquare(number) {
@@ -45,25 +49,25 @@ function generateSquare(number) {
 
     //evento del colore sui quadrati
     newSquare.addEventListener('click', function() {
-    this.classList.add('blue');
-    alert(number);
+    //this.classList.add('blue');
+    //alert(number);
+    if (this === bombsArray) {
+        this.classList.add ('red')
+    } else {
+        this.classList.add ('blue')
+    }
     });
 
     return newSquare;
 };
+
+});
+
+
 
 //funzione che crea un numero random
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-//PARTE DUE
-//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-//
-//
-//
-//
-//
+
